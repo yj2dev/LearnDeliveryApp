0638d49a6d2bf51af5cb1814763e39ea95e80cf5
adu <nogo22@naver.com>
2022-07-13 17:23:10 +0900

    init the apps initalize

---
+++ src/pages/Delivery.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Delivery() {
+  return (
+    <View>
+      <Text>배달</Text>
+    </View>
+  );
+}
+export default Delivery;
---
+++ src/pages/Orders.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Orders() {
+  return (
+    <View>
+      <Text>주문</Text>
+    </View>
+  );
+}
+export default Orders;
